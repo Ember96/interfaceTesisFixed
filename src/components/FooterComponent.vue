7df5e96145ca9024d01bf784145e7dba482fa121
Ember96 <gnomeaddict96@gmail.com>
2023-02-08 05:35:08 -0800

    new file:   dist/css/about.8fc2c8b5.css 	renamed:    dist/css/chunk-vendors.2186fbdd.css -> dist/css/chunk-vendors.b7cf66b7.css 	modified:   dist/index.html 	new file:   dist/js/about.2e1f5f68.js     new file:   dist/js/about.2e1f5f68.js.map 	deleted:    dist/js/about.89acf825.js 	deleted:    dist/js/about.89acf825.js.map 	deleted:    dist/js/app.0d837220.js 	deleted:    dist/js/app.0d837220.js.map 	new file:   dist/js/app.ec63a8e0.js 	new file:   dist/js/app.ec63a8e0.js.map 	renamed:    dist/js/chunk-vendors.4c4a7ad3.js -> dist/js/chunk-vendors.255e54f4.js 	new file:   dist/js/chunk-vendors.255e54f4.js.map 	deleted:    dist/js/chunk-vendors.4c4a7ad3.js.map 	modified:   src/components/FooterComponent.vue

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -5,23 +5,17 @@
   >
     <v-row
       justify="center"
+      class="text-center white--text"
       no-gutters
-    >
-      <v-btn
-        v-for="link in links"
-        :key="link"
-        color="white"
-        text
-        rounded
-        class="my-2"
-      >
-        {{ link }}
-      </v-btn>
+    >Este sistema no está pensado para su uso fuera del ambiente de pruebas de viabilidad
+     con la participación de los estudiantes de la asignatura Ciencia de Datos para Sensores
+      del Dpto. de Ciencias Computacionales.<br>Absténgase de emplearlo con cualquier otro fin 
+      y conste que las interacciones con el mismo generan registro en propiedad de la institución a pie de página.
       <v-col
         class="primary lighten-2 py-4 text-center white--text"
         cols="12"
       >
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        {{ new Date().getFullYear() }} — <strong>CICESE</strong>
       </v-col>
     </v-row>
   </v-footer>
